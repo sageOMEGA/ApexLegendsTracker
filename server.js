b3017527cf932cc080497fc531216ce2b5a447b4
sageOMEGA <abhishekdhakad61@gmail.com>
2020-06-06 10:59:59 +0530

    Deploying

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const path = require("path");
 
 //Load ENV
 dotenv.config({ path: "./config.env" });
@@ -19,6 +20,15 @@ if (process.env.NODE_ENV === "development") {
 
 //Profile Routes
 app.use("/api/v2/profile", require("./routes/profile"));
+
+// Handle Prdction
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 8000;
 
