8272fee17dc8b4c50fa05696dcaef2ea294c56d4
sageOMEGA <abhishekdhakad61@gmail.com>
2020-07-10 10:14:35 +0530

    Extra Stuff

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Navigaton from "./components/layouts/Navigaton";
 import Home from "./components/pages/Home";
 import Gamer from "./components/gamers/Gamer";
+import NotFound from "./components/pages/NotFound";
 import ApexState from "./components/context/ApexState";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
@@ -15,6 +16,7 @@ const App = () => {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/:platform/:gamerId" component={Gamer} />
+            <Route component={NotFound} status={404} />
           </Switch>
         </Fragment>
       </Router>
---
+++ client/src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Player Not Found</h1>
+      <h3>Please Enter Correct Details</h3>
+    </div>
+  );
+};
+
+export default NotFound;
